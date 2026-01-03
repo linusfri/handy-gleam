ad34571bb6c242e38651d84445ab94987003f4ad
linus <linus.f@protonmail.ch>
2026-01-03 15:32:08 +0100

    Added nginx configuration to auth server

--- devenv.nix
+++ devenv.nix
@@ -1,10 +1,14 @@
 {
   pkgs,
   inputs,
+  config,
   ...
 }:
 let
   gleamPkgs = import inputs.gleam-pkgs { system = pkgs.stdenv.system; };
+  domain = "auth-server.local";
+
+  inherit (import ./nix_modules/nginx/locations.nix) root;
 in
 {
   imports = [
@@ -13,7 +17,9 @@ in
   ];
 
   config = {
-    hosts."auth-server.local" = "127.0.0.1";
+
+    hosts."${domain}" = "127.0.0.1";
+    certificates = builtins.attrNames config.hosts;
 
     packages = with pkgs; [
       git
@@ -40,6 +46,40 @@ in
       process-compose = {
         depends_on.postgres.condition = "process_healthy";
       };
+    };
+
+    services.nginx = {
+      enable = true;
+      httpConfig = ''
+        server {
+          listen 8080;
+          server_name ${domain};
+
+          location / {
+            ${root}
+          }
+
+          client_max_body_size 100M;
+        }
+
+        server {
+          listen 4443 ssl http2;
+          server_name ${domain};
+
+          ssl_certificate ${config.env.DEVENV_STATE}/mkcert/${domain}.pem;
+          ssl_certificate_key ${config.env.DEVENV_STATE}/mkcert/${domain}-key.pem;
+          
+          ssl_protocols TLSv1.2 TLSv1.3;
+          ssl_ciphers HIGH:!aNULL:!MD5;
+          ssl_prefer_server_ciphers on;
+
+          location / {
+            ${root}
+          }
+
+          client_max_body_size 100M;
+        }
+      '';
     };
 
     languages.gleam = {
